9883fb279e2579b557391b932ccabc7adbf85be0
KenJonesPizza <kenjwho@gmail.com>
2026-03-30 15:10:21 -0500

    email commit

---
+++ email-marketing/frontend/sanity/queries.ts
@@ -0,0 +1,23 @@
+import {defineQuery} from 'next-sanity'
+
+export const allPostsQuery = defineQuery(`
+  *[_type == "post" && defined(slug.current)] | order(_createdAt desc) {
+    _id,
+    title,
+    "slug": slug.current,
+  }
+`)
+
+export const postQuery = defineQuery(`
+  *[_type == "post" && slug.current == $slug] [0] {
+    _id,
+    title,
+    "slug": slug.current,
+    body,
+  }
+`)
+
+export const postSlugsQuery = defineQuery(`
+  *[_type == "post" && defined(slug.current)]
+  {"slug": slug.current}
+`)
